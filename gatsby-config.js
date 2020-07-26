2fe39d159dcfb6caf2aad4568f2500471a6fbc23
KoukiIijima <lle0nonnndebo@gmail.com>
2020-07-26 13:02:50 +0900

    [add]remark, sass, filesystem, GraphQL IDE plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,20 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
         path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -42,7 +50,7 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 590,
+              maxWidth: 640,
             },
           },
         ],
